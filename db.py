73cdbfc15e602a25faeb468aace5846f243b967a
Manoj Kumar <kvssmk36@gmail.com>
2025-06-06 01:02:28 +0530

    Added mongodb uri string with USERNAME AND PASSWORD

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from pymongo import AsyncMongoClient
 
-client = AsyncMongoClient("mongodb://localhost:27017/")
+uri = "mongodb+srv://f20240236:*s13634151e*@locosync.r0qmmi1.mongodb.net/?retryWrites=true&w=majority&appName=locosync"
+client = AsyncMongoClient(uri)
 db = client["carpool"]
 
 # This provides a shared db across all files
